0cd3a24987ab6bef19b288d78f6c8b0c8b61eed5
Jeffrey Jones <jeff@jones.be>
2014-06-13 21:42:51 +0900

    Remove redundant encoding step.
    Remove a redundant force_encoding after already encoding the string.
    The encode contains all the error handling we need

--- lib/ankusa/extensions.rb
+++ lib/ankusa/extensions.rb
@@ -1,5 +1,5 @@
 class String
   def to_ascii
-    encode("UTF-8", :invalid => :replace, :undef => :replace, :replace => "").force_encoding('UTF-8') rescue ""
+    encode("UTF-8", :invalid => :replace, :undef => :replace, :replace => "")
   end
 end
